bbf31d80abe113bbb33e63636cae1983eb0809d7
gwolak <grzegorzwolak97@gmail.com>
2022-11-05 09:19:17 +0100

    added skills section

---
+++ js/slick.js
@@ -0,0 +1,13 @@
+$('.aboutme__carousel').slick({
+	arrows: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	mobileFirst: true,
+	autoplaySpeed: 2000,
+	pauseOnFocus: false,
+	prevArrow:
+		'<button type="button" class="slick-prev"><i class="fa-sharp fa-solid fa-chevron-left"></i></button>',
+    nextArrow:
+        '<button type="button" class="slick-next"><i class="fa-sharp fa-solid fa-chevron-right"></i></button>'
+});
